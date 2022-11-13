b85913281d175c5af5375e4ed0262b8ad79d1602
yunsuklee <sergioleesp@gmail.com>
2022-11-13 19:26:07 +0100

    feat: added messages component and service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,5 @@ export class HeroService {
     return heroes;
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,25 +10,27 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  heroes: Hero[] = [];
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
   selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService, private messageService: MessageService) { } // private heroService property and defines it as an injection
+  
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
-  constructor(private heroService: HeroService) { } // private heroService property and defines it as an injection
-  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
   getHeroes():void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
   
 }
