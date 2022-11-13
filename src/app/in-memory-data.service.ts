d6ea2c5616ed983a31772aff9c134d79123834aa
yunsuklee <sergioleesp@gmail.com>
2022-11-13 20:47:26 +0100

    refactor: updated default heroes list

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,15 +8,15 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 12, name: 'Dr. Nice' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr. IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 1, name: 'Iron Man' },
+      { id: 2, name: 'Thor' },
+      { id: 3, name: 'Wasp' },
+      { id: 4, name: 'Ant-Man' },
+      { id: 5, name: 'Hulk' },
+      { id: 6, name: 'Captain America' },
+      { id: 7, name: 'Hawkeye' },
+      { id: 8, name: 'Quicksilver' },
+      { id: 9, name: 'Scarlet Witch' }
     ];
     return {heroes};
   }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,9 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 1, name: 'Iron Man' },
+  { id: 2, name: 'Thor' },
+  { id: 3, name: 'Wasp' },
+  { id: 4, name: 'Ant-Man' },
+  { id: 5, name: 'Hulk' }
 ];
